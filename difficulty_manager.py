82d1eecc01fa3b7107c3086ebd294f9aa2ce39ee
Jacuzzi <jheklayag@gmail.com>
2025-06-21 19:24:39 +0800

    Added words to the easy difficulty

--- difficulty_manager.py
+++ difficulty_manager.py
@@ -1,2 +1,11 @@
 # Create a class to manage different difficulties
 class DifficultyManager:
+    def __init__(self):
+        self.word_levels = {
+            "easy": [
+                "code", "data", "loop", "func", "var", "int", "str", "list", "dict", "set",
+                "tuple", "def", "class", "True", "False", "None", "if", "else", "elif", "for",
+                "while", "try", "except", "pass", "break", "cont", "ret", "arg", "kwarg", "self",
+                "init", "main", "path", "file", "read", "write", "open", "close", "sort", "map",
+                "filt", "iter", "next", "call", "obj", "type", "cast", "comp", "pip", "env"
+            ],
