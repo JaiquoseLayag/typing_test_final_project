1eb554d087f6b7441fb2334f6e1fbdbb888d33ec
Jacuzzi <jheklayag@gmail.com>
2025-06-21 18:06:07 +0800

    Removed the WordGenerator class to move to a different file

--- typing_test.py
+++ typing_test.py
@@ -1,24 +1,7 @@
 # Import necessary built-in modules
-import random
 import time 
 import tkinter as tk
 
-# Define a class to manage and return random words
-class WordGenerator:
-    def __init__(self):
-# Store word list
-        self.words = [
-            'python', 'keyboard', 'function', 'variable', 'loop', 'syntax', 'object',
-            'method', 'inheritance', 'class', 'string', 'integer', 'boolean', 'tuple',
-            'list', 'dictionary', 'module', 'package', 'exception', 'lambda', 'return',
-            'encapsulation', 'recursion', 'framework', 'backend', 'frontend',
-            'compiler', 'interpreter', 'debugging', 'iteration', 'algorithm',
-            'development', 'deployment', 'parameter', 'argument', 'constructor',
-            'destructor', 'virtual', 'static', 'mutable'
-        ]
-# Return one random word each time
-    def get_word(self):
-        return random.choice(self.words)
 
 # Define a class to handle result calculation
 class Result:
